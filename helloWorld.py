8ee21c4d73980a7d3d02f8e172ca2ca165c0a1e2
fi-nage <sophie.egan3@icloud.com>
2024-03-27 21:16:14 +0000

    add a loop to the program

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,22 @@
-# Taking input from the user
-userInput = input("Please enter something: ")
-# Printing the input provided by the user
-print("You entered:", userInput)
+import random
+
+# Function to generate a random color in RGB format
+def randomColor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+while True:
+    # Taking input from the user
+    userInput = input("Please enter something: ")
+
+    if userInput.lower() == 'exit':
+        print("Exiting the program...")
+        break
+
+    # Printing the input provided by the user with random colors for each letter
+    coloredOutput = ""
+    for letter in userInput:
+        r, g, b = randomColor()
+        coloredOutput += f"\033[38;2;{r};{g};{b}m{letter}\033[0m"  # ANSI escape sequence for color
+    print("You entered:", coloredOutput)
