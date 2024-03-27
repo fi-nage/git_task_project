82007e0b5ff2c6c14fe0b7f1d2cadcebf24ce58e
fi-nage <sophie.egan3@icloud.com>
2024-03-27 20:59:18 +0000

    adding random colors to printed message

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,18 @@
+import random
+
+# Function to generate a random color in RGB format
+def randomColor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
 # Taking input from the user
 userInput = input("Please enter something: ")
-# Printing the input provided by the user
-print("You entered:", userInput)
+
+# Printing the input provided by the user with random colors for each letter
+coloredOutput = ""
+for letter in userInput:
+    r, g, b = randomColor()
+    coloredOutput += f"\033[38;2;{r};{g};{b}m{letter}\033[0m"  # ANSI escape sequence for color
+print("You entered:", coloredOutput)
